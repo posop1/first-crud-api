ca5c259d7f619dc9663a856b187ec2ccc4282a3a
posop1 <artur.yakhin52@gmail.com>
2022-08-23 15:47:42 +0500

    init

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+const middleWare = express.json();
+
+app.use(middleWare);
+
+const db = {
+  lib: [
+    { id: 1, title: 'react js' },
+    { id: 2, title: 'vue' },
+    { id: 3, title: 'angular' },
+    { id: 4, title: 'vue' },
+    { id: 5, title: 'redux js' },
+    { id: 6, title: 'svetle' },
+  ],
+};
+
+app.get('/api', (req, res) => {
+  let found = db.lib;
+  if (req.query.title) {
+    found = found.filter((obj) => obj.title.indexOf(req.query.title as string) > -1);
+  }
+
+  res.json(found);
+});
+
+app.get('/api/:id', (req, res) => {
+  const find = db.lib.find((obj) => obj.id === +req.params.id);
+
+  if (!find) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.json(find);
+});
+
+app.post('/api', (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const createLib = {
+    id: +new Date(),
+    title: req.body.title,
+  };
+
+  db.lib.push(createLib);
+  res.json(createLib);
+});
+
+app.delete('/api/:id', (req, res) => {
+  db.lib = db.lib.filter((obj) => obj.id !== +req.params.id);
+
+  res.sendStatus(204);
+});
+
+app.put('/api/:id', (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const find = db.lib.find((obj) => obj.id === +req.params.id);
+
+  if (!find) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  find.title = req.body.title;
+
+  res.json(find);
+});
+
+app.listen(port, () => {
+  console.log(`listen on ${port}`);
+});
